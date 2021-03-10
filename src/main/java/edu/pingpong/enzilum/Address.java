9003e60e38296e960b91ea19f4018a88c7616a1c
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-10 11:48:48 +0100

    feat(Address): setup Attributes, Constructor, Getters and Setters and generateKeyPair method

---
+++ src/main/java/edu/pingpong/enzilum/Address.java
@@ -0,0 +1,60 @@
+package edu.pingpong.enzilum;
+
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class Address {
+
+    // Attributes
+    private PublicKey PK = null;
+    private PrivateKey SK = null;
+    private double balance = 0.0d;
+    private final String SYMBOL = "EZI";
+
+    // Constructor
+    public Address(){};
+
+    // Getters & Setters
+    public PublicKey getPK() {
+        return PK;
+    }
+
+    public void setPK(PublicKey PK) {
+        this.PK = PK;
+    }
+
+    public PrivateKey getSK() {
+        return SK;
+    }
+
+    public void setSK(PrivateKey SK) {
+        this.SK = SK;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getSYMBOL() {
+        return SYMBOL;
+    }
+
+    // Method generateKeyPair, responsible for generating and assigning a public and private key to Address.
+    public void generateKeyPair() {
+        KeyPair pair = GenSig.generateKeyPair();
+        assert pair != null;
+        setPK(pair.getPublic());
+        setSK(pair.getPrivate());
+    }
+
+    @Override
+    public String toString(){
+        return "Dirección Pública: " + getPK().hashCode() + '\n' +
+                "Balance: " + getBalance() + '\n';
+    }
+}
